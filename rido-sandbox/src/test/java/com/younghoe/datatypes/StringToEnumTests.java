1061b70e94671103bd0102a6e5517d59ee61a2bd
Tony Younghoe Ahn <ahnyounghoe@gmail.com>
2016-01-26 03:07:21 +0900

    String to Enum Conversion Study

---
+++ rido-sandbox/src/test/java/com/younghoe/datatypes/StringToEnumTests.java
@@ -0,0 +1,28 @@
+package com.younghoe.datatypes;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.fail;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by tony on 2016. 1. 26..
+ */
+public class StringToEnumTests {
+
+    @Test
+    public void string_to_enum(){
+        assertEquals(RevenueOrExpense.수입, RevenueOrExpense.valueOf("수입"));
+        assertEquals(RevenueOrExpense.지출, RevenueOrExpense.valueOf("지출"));
+        try{
+            RevenueOrExpense.valueOf("없는 것..");
+            fail();
+        }catch (IllegalArgumentException e){
+            e.printStackTrace();
+        }
+    }
+
+    enum RevenueOrExpense{
+        수입, 지출
+    }
+}
