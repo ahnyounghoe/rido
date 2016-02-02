4618236ef1505a991125739917b0011251f1505c
Tony Younghoe Ahn <ahnyounghoe@gmail.com>
2016-02-02 14:02:12 +0900

    #1 Refactor java-sandbox: add joda-money study artifacts

--- sandbox/java-sandbox/src/test/java/com/younghoe/datatypes/MoneyTypeTests.java
+++ sandbox/java-sandbox/src/test/java/com/younghoe/datatypes/MoneyTypeTests.java
@@ -1,5 +1,6 @@
 package com.younghoe.datatypes;
 
+import junit.framework.TestCase;
 import org.joda.money.CurrencyUnit;
 import org.joda.money.Money;
 import org.junit.Test;
@@ -23,13 +24,11 @@ public class MoneyTypeTests {
      *     <ul>
      *         <li>@{link java.util.Currency Currency}</li> 클래스로 대한민국 원 표시</li>
      *         <li>@{link java.math.BigDecimal BigDecimal} 간단히 써보기</li>
-     *         <li>@{link org.joda.money.Money} 사용법 확인</li>
      *         <li>외부 라이브러리 없이 간단하게 통화 데이터를 표현하는 방법</li>
      *     </ul>
      * </p>
      *
      * @see <a href="http://blog.eisele.net/2011/08/working-with-money-in-java.html">java.util.Currency</a>
-     * @see <a hef="https://github.com/JodaOrg/joda-money/blob/master/src/main/java/org/joda/money/BigMoney.java">Joda-Money</a>
      * @see <a href="https://docs.oracle.com/javase/tutorial/java/data/numberformat.html">Java Tutorial</a>
      * @see <a href="https://en.wikipedia.org/wiki/ISO_4217">ISO 4217</a>
      */
@@ -63,6 +62,18 @@ public class MoneyTypeTests {
         System.out.println("Max of Int: " + Integer.MAX_VALUE);
         System.out.println("Max of Long: " + Long.MAX_VALUE);
 
+    }
+
+    /**
+     * <p>
+     *     @{link org.joda.money.Money} 사용법 확인
+     * </p>
+     *
+     * @see <a href="http://www.joda.org/joda-money/">Joda-Money User Guide</a>
+     * @see <a hef="https://github.com/JodaOrg/joda-money/blob/master/src/main/java/org/joda/money/BigMoney.java">Joda-Money</a>
+     */
+    @Test
+    public void using_joda_money(){
         // Use Joda-money
         Money money = Money.of(CurrencyUnit.of("KRW"), new BigDecimal(30000.0));
         assertEquals(30000, money.getAmountMajorInt());
@@ -73,6 +84,34 @@ public class MoneyTypeTests {
         assertNotSame("₩", money.getCurrencyUnit().getSymbol()); // WARN!
         assertEquals("\uFFE6", money.getCurrencyUnit().getSymbol());
         assertNotSame("\u20A9", money.getCurrencyUnit().getSymbol()); // WARN!
+
+        // create a monetary value
+        money = Money.parse("USD 23.87");
+        System.out.println(money);
+
+        // Same as USd
+        System.out.println(Money.parse("KRW 2000.0"));
+
+        // add another amount with safe double conversion
+        CurrencyUnit usd = CurrencyUnit.of("USD");
+        System.out.println(usd);
+
+        CurrencyUnit krw = CurrencyUnit.of("KRW");
+        TestCase.assertEquals("￦", krw.getSymbol());
+        TestCase.assertEquals("KRW", krw.getCurrencyCode());
+        TestCase.assertEquals("KRW", krw.getCode());
+        TestCase.assertEquals(0, krw.getDecimalPlaces());
+        TestCase.assertEquals(CurrencyUnit.class, krw.getClass());
+        TestCase.assertEquals(false, krw.isPseudoCurrency());
+        TestCase.assertEquals("410", krw.getNumeric3Code());
+        TestCase.assertEquals(410, krw.getNumericCode());
+        TestCase.assertEquals(Currency.getInstance(Locale.KOREA), krw.toCurrency());
+
+        for(String countryCode : krw.getCountryCodes())
+            System.out.println("countryCode: " + countryCode);
+
+        for(String countryCode : CurrencyUnit.of("JPY").getCountryCodes())
+            System.out.println("countryCode: " + countryCode);
 
     }
 
